8c5b65a74f84f74595057d77b5475240a854cd17
Tanmayee Kamath <30297258+tanmayeekamath@users.noreply.github.com>
2022-05-19 17:35:39 +0000

    minor update

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 app.post("/heart", (req, res) => {
   db.query(
     "UPDATE haikus SET hearts = hearts + 1 WHERE id = $1",
-    [req.body.id],
+    [req.body.idz],
     () => res.send("Success")
   );
 });
